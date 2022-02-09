c3080bf098dca34d9ea6574762405829d17cd6b0
César Milesi <cesar.milesi@epita.fr>
2022-02-09 19:40:23 +0100

    Test : Created LauncherTest

---
+++ search-api/src/test/java/fr/lernejo/search/api/LauncherTest.java
@@ -0,0 +1,13 @@
+package fr.lernejo.search.api;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LauncherTest {
+
+    @Test
+    void main() {
+        Launcher.main(new String[]{});
+    }
+}
